7839d98a9f429525ec7f75d75a73c4ed0669a74a
panucitelcz <petr.b257@seznam.cz>
2023-11-29 18:17:13 +0100

    Hotovo

--- script/dist/script.dev.js
+++ script/dist/script.dev.js
@@ -13,9 +13,12 @@ function VypisDoTabulky() {
     var tab_jmeno = newRow.insertCell(0);
     var tab_prijmeni = newRow.insertCell(1);
     var tab_rocnik = newRow.insertCell(2);
+    var tab_id = newRow.insertCell(3);
     tab_jmeno.innerHTML = seznam[i].Jmeno;
     tab_prijmeni.innerHTML = seznam[i].Prijmeni;
     tab_rocnik.innerHTML = seznam[i].Rocnik;
+    var cislo = seznam[i].Id;
+    tab_id.innerHTML = cislo; // Opraveno přiřazení
   }
 }
 
@@ -24,13 +27,21 @@ function Pridat() {
   var prijmeni = document.getElementById("surname");
   var rocnik = document.getElementById("rocnik");
 
-  if (jmeno.value != "" && isNaN(jmeno.value)) {
-    if (prijmeni.value != "" && isNaN(prijmeni.value)) {
-      if (rocnik.value != "" && !isNaN(rocnik.value) && rocnik.value > 0 && rocnik.value < 10) {
+  if (jmeno.value !== "" && isNaN(jmeno.value)) {
+    if (prijmeni.value !== "" && isNaN(prijmeni.value)) {
+      if (rocnik.value !== "" && !isNaN(rocnik.value) && rocnik.value > 0 && rocnik.value < 10) {
+        var cislo = "";
+
+        for (var i = 0; i < 5; i++) {
+          var Nahodne_cislo = Math.floor(Math.random() * 10);
+          cislo += Nahodne_cislo;
+        }
+
         seznam.push({
           Jmeno: jmeno.value,
           Prijmeni: prijmeni.value,
-          Rocnik: rocnik.value
+          Rocnik: rocnik.value,
+          Id: cislo
         });
         VypisDoTabulky();
       }
--- script/script.js
+++ script/script.js
@@ -13,32 +13,40 @@ function VypisDoTabulky() {
         let tab_jmeno = newRow.insertCell(0);
         let tab_prijmeni = newRow.insertCell(1);
         let tab_rocnik = newRow.insertCell(2);
+        let tab_id = newRow.insertCell(3);
 
         tab_jmeno.innerHTML = seznam[i].Jmeno;
         tab_prijmeni.innerHTML = seznam[i].Prijmeni;
         tab_rocnik.innerHTML = seznam[i].Rocnik;
+        let cislo = seznam[i].Id;
+        tab_id.innerHTML = cislo; // Opraveno přiřazení
     }
 }
 
-function Pridat(){
+function Pridat() {
     let jmeno = document.getElementById("name");
     let prijmeni = document.getElementById("surname");
     let rocnik = document.getElementById("rocnik");
 
-    if((jmeno.value != "") && (isNaN(jmeno.value))){
-        
-        if((prijmeni.value != "") && (isNaN(prijmeni.value))){
+    if (jmeno.value !== "" && isNaN(jmeno.value)) {
 
-            if((rocnik.value != "") && (!isNaN(rocnik.value)) && (rocnik.value>0) && (rocnik.value<10)){
-                
-                seznam.push({Jmeno: jmeno.value, Prijmeni: prijmeni.value, Rocnik: rocnik.value});
+        if (prijmeni.value !== "" && isNaN(prijmeni.value)) {
+
+            if (rocnik.value !== "" && !isNaN(rocnik.value) && rocnik.value > 0 && rocnik.value < 10) {
+
+                let cislo = "";
+                for (let i = 0; i < 5; i++) {
+                    let Nahodne_cislo = Math.floor(Math.random() * 10);
+                    cislo += Nahodne_cislo;
+                }
+
+                seznam.push({ Jmeno: jmeno.value, Prijmeni: prijmeni.value, Rocnik: rocnik.value, Id: cislo });
+
                 VypisDoTabulky();
             }
         }
-    }
-    else
-    {
-        alert("Jsi si jisty, že nejsi opilý?")
+    } else {
+        alert("Jsi si jisty, že nejsi opilý?");
     }
 
     console.log(seznam);
